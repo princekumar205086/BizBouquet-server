7cbe85595cbb7e5ffe20a200b774ba9c8c362889
princesatya <prince_kumar_mca2021@jimsindia.org>
2023-01-09 22:45:19 +0530

    Mongo atlas connection added

--- src/db/connection/connection.js
+++ src/db/connection/connection.js
@@ -1,6 +1,17 @@
 const mongoose = require("mongoose");
-const connection = mongoose.connect("mongodb://localhost:27017/bd-api",{}).then(()=>{
+
+// offline configration
+// const connection = mongoose.connect("mongodb://localhost:27017/bd-api",{}).then(()=>{
+//     console.log("connection successful");
+// }).catch((e)=>{
+//     console.log("something went wrong!");
+// })
+
+// mongodb cloud configration
+
+const connection = mongoose.connect("mongodb+srv://princeraj:prince123@cluster0.agloup8.mongodb.net/bd-api?retryWrites=true&w=majority",{}).then(()=>{
     console.log("connection successful");
 }).catch((e)=>{
     console.log("something went wrong!");
 })
+
